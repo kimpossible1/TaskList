07860236d7be4f1d5a038aa7a6675a777e0a2d06
Kimberley Zell <kimberley.zell@gmail.com>
2017-09-20 19:09:09 -0700

    added html.erb for new task and also added def new to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date], complete: false)
+  end
 end
