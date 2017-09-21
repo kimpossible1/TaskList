434b29f6739e921564dec110aee7958cabebdcbd
Kimberley Zell <kimberley.zell@gmail.com>
2017-09-21 10:30:32 -0700

    got edit working. added delete button and method - works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,17 +29,37 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    if @task.update(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date])
-      redirect_to tasks_path
-    else
-      render :edit
-    end
+
+    @task.title = params[:task][:title]
+    @task.description = params[:task][:description]
+    @task.due_date = params[:task][:due_date]
+
+    @task.save
+
+    redirect_to task_path(@task)
+
+    #book_updates = (params[:task]
+    #[:task][:title], params[:task][:description],  params[:task][:due_date])
+    #
+    # @task.update_attributes(book_updates)
+    # if @task.update(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date])
+    #   redirect_to show_task_path(@task.id)
+    # else
+    #   render :edit
+    # end
   end
 
 
   def complete
     task = Task.find(params[:id])
     task.complete!
+    redirect_to tasks_path
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.delete
+
     redirect_to tasks_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   get('/tasks/new', to: 'tasks#new', as: 'new_task')
   post('/tasks', to: 'tasks#create')
   get('/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task')
-  patch('/tasks/:id/edit', to: 'tasks#update', as: 'update_task')
-  get('/tasks/:id', to: 'tasks#show', as: 'show_task')
+  patch('/tasks/:id', to: 'tasks#update', as: 'update_task')
+  get('/tasks/:id', to: 'tasks#show', as: 'task')
   patch('tasks/:id/complete', to: 'tasks#complete', as:'complete_task')
+  patch('tasks/:id/delete', to: 'tasks#delete', as: 'delete_task')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
